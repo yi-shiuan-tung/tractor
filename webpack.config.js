6a700af9f853bd9f59d629d530146feb88337c3b
Yi-Shiuan Tung <ytung@mit.edu>
2020-04-04 18:13:00 -0400

    add basic settings for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
+module.exports = [{
+  entry: './ts/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
+}];
